62e3829698d5d9c536735061875a1bef5eeaad52
CantheProptimzier <can.say@proptimize.co>
2023-10-09 16:27:15 -0400

    projecting

--- src/components/Collaborators/Collaborators.js
+++ src/components/Collaborators/Collaborators.js
@@ -8,7 +8,7 @@ export default function Email() {
       <Button
         pX={20}
         pY={12}
-        href="https://example.com"
+        href="https://ldpt2m03ygt.typeform.com/to/EmUw5jhw"
         style={{ background: '#000', color: '#fff' }}
       >
         Click me
