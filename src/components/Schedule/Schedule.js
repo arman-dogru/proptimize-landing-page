e7c027418c09d056ad65b8c1be731f316c93d16d
CantheProptimzier <can.say@proptimize.co>
2023-10-09 19:07:51 -0400

    added 3 slots to founders, changed font of FAQ

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -10,20 +10,22 @@ function Schedule() {
   return (
     <div id="Pricing" className="Pricing">
       <div id="pricingTitle" className="pricingTitle">FOUNDERS</div>
-      <Stack>
-        <div className="flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front">
-              <img src="img_avatar.png" alt="Avatar" style={{ width: '300px', height: '300px' }} />
-            </div>
-            <div className="flip-card-back">
-              <h1>John Doe</h1>
-              <p>Architect & Engineer</p>
-              <p>We love that guy</p>
+      <div className="founders-container">
+        {[1, 2, 3].map(index => (
+          <div key={index} className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src="img_avatar.png" alt="Avatar" style={{ width: '300px', height: '300px' }} />
+              </div>
+              <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect & Engineer</p>
+                <p>We love that guy</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Stack>
+        ))}
+      </div>
     </div>
   );
 }
