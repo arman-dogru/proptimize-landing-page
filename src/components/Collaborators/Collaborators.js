a8e1afa4cad5f37addfa64fa2f5bfdf72fba904f
CantheProptimzier <can.say@proptimize.co>
2023-10-10 22:40:12 -0400

    button and text on top of it added

--- src/components/Collaborators/Collaborators.js
+++ src/components/Collaborators/Collaborators.js
@@ -3,14 +3,18 @@ import './Collaborators.css';
 
 function Collaborators() {
   return (
-    <a
-      className="Button luxuryButton"
-      href="https://ldpt2m03ygt.typeform.com/to/EmUw5jhw"
-    >
-      Join our exclusive community of valued users and stay connected with us <br /> 
-      by taking just a few seconds to complete the form below. <br /> 
-    </a>
-  );
+    <div className="container">  
+      <div className="infoBox">
+      Join our exclusive community of valued users and stay connected with us by taking just a few seconds to complete the form below.
+      </div>
+      <a
+        className="Button luxuryButton"
+        href="https://ldpt2m03ygt.typeform.com/to/EmUw5jhw"
+      >
+        REGISTER HERE
+      </a>
+    </div>  
+  );
 }
 
 export default Collaborators;
